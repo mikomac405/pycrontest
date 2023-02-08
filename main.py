e1e997f9a3f767763fed3e6f0b81c1dcd4e8eee6
Dadoheh <56519168+Dadoheh@users.noreply.github.com>
2023-02-08 23:51:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import dearpygui.dearpygui as dpg
+
+dpg.create_context()
+
+
+def callback(sender, data):
+    print("Button Pressed")
+
+
+with dpg.window(label="Tutorial", pos=(200, 200), width=800, height=200) as win1:
+    dpg.add_text("Type cron: 'At 04:05.'")
+    dpg.add_input_text(default_value="Type your answer here", width=270, height=750)
+    dpg.add_button(label="submit", callback=callback)
+
+
+dpg.create_viewport(title='Custom Title', width=1330, height=500)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
 
